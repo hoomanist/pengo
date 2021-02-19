37b7fbe2c1253efed6f5a7eae8bb79a24a582915
hoomanist <hham.group12@gmail.com>
2021-02-19 15:56:21 +0330

    draw a line

---
+++ line.go
@@ -0,0 +1,26 @@
+package pengo
+
+import (
+	"image/color"
+)
+
+type Line struct {
+	X1        int
+	X2        int
+	Y         int
+	Direction string
+	Color     color.Color
+}
+
+func (l *Line) Draw(c *Canvas) {
+	if l.Direction == "Horizontal" {
+		for ; l.X1 <= l.X2; l.X1++ {
+			c.img.Set(l.X1, l.Y, l.Color)
+		}
+	} else if l.Direction == "Vertical" {
+		for ; l.X1 <= l.X2; l.X1++ {
+			c.img.Set(l.Y, l.X1, l.Color)
+		}
+	}
+
+}
